8588a4024c063bb292f449f86fbf5502dee0ac52
Grant Barnes <gfbarnes78@gmail.com>
2024-01-28 19:35:33 -0600

    Revert "adding extra line erase on select erase after"
    This reverts commit 7b070e35800e04da1d16cfa1e88be2b85464cc0f.

--- src/prompts/select.rs
+++ src/prompts/select.rs
@@ -144,7 +144,6 @@ impl Select {
                 lines_to_erase += 1;
             }
             for _ in 0..lines_to_erase {
-                ansi::erase::line();
                 ansi::cursor::previous_line();
                 ansi::erase::line();
             }
